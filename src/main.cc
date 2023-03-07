025354fc3ab1d1b316a63eee15de4e67d1eec2f8
jacob cyr <jacobncyr@outlook.com>
2023-03-07 08:14:13 -0600

    asking chatgpt why im stuck infinite loop

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,10 @@
 #include<unistd.h>
 #include "processtheargs.h"
 
-
+//process -- read strings from stin and process them
+//returns EXIT_SUCCESS on successfull completion
+//returns EXIT_FAILURE on unsuccefful completion 
+int process(void);
 
 int main(int argc, char *argv[]){
 	int opt;
@@ -13,11 +16,23 @@ int main(int argc, char *argv[]){
 		switch(opt){
 			case 'h':
 				printf("h case triggerred");
+				return EXIT_SUCCESS;
 				break;
 			default:
 				printf("drfault case triggerred");
+				return EXIT_FAILURE;
 				break;
 
 	}
-	return EXIT_SUCCESS;
-	}}
+	}
+	return process();
+	}
+//CMD_SIZE -- assumed size for an input string/comand
+const int CMD_SIZE = 80;
+
+int process(void){
+	char cmd[CMD_SIZE + 1];//add space for the tombstone
+	while(0==feof(stdin)){
+		if(0 != scanf("%s",cmd)){
+			printf("%s\n",cmd);
+		}}return EXIT_SUCCESS;}
