aa834ab0cee8d6d161bd6172e0f1ada7a954fd9f
jacob cyr <jacobncyr@outlook.com>
2023-03-09 15:51:38 -0600

    got the functions from stack linked into the c program

--- src/stack.cc
+++ src/stack.cc
@@ -4,5 +4,29 @@
 
 int main(void){
 	printf("test");
+	push(2.0);
+	pop();
+	empty();
+	peek();
 	return EXIT_SUCCESS;
 }
+
+bool push(float){
+	printf("push works");
+	return true;;
+}
+
+float pop(void){
+	printf("pop works");
+	return 1.0;
+}
+bool empty(void){
+	printf("empty works");
+	return true;
+}
+float peek(void){
+	printf("peek works");
+	return 2.0;
+}
+
+
