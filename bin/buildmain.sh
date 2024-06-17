519e8aad3d16ed0c2ab2b0b6388ae9c1dab0cbee
jacob cyr <jacobncyr@outlook.com>
2024-06-17 10:35:03 -0600

    stiched to windows and deleted GPT generated crap

--- bin/buildmain.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-g++ -pedantic -Wall -std=c++17 -c -o bin/main.o src/main.cc
-g++ -pedantic -Wall -std=c++17 -c -o bin/processtheargs.o src/processtheargs.cc
-g++ -pedantic -Wall -std=c++17 -o bin/maintest bin/main.o bin/processtheargs.o
--- src/main.cc
+++ src/main.cc
@@ -1,49 +1,7 @@
-#include<cstdlib>
-#include<cstdio>
-#include<cassert>
-#include<unistd.h>
-#include "processtheargs.h"
+#include <cstdio>
+#include <cstdlib>
 
-//process -- read strings from stin and process them
-//returns EXIT_SUCCESS on successfull completion
-//returns EXIT_FAILURE on unsuccefful completion 
-int process(void);
-
-int main(int argc, char *argv[]){
-	int opt;
-	while (( opt = getopt(argc, argv,"h")) != -1){
-	
-		switch(opt){
-			case 'h':
-				printf("h case triggerred");
-				return EXIT_SUCCESS;
-				break;
-			default:
-				printf("drfault case triggerred");
-				return EXIT_FAILURE;
-				break;
-
-	}
-	}
-	return process();
-	}
-//CMD_SIZE -- assumed size for an input string/comand
-const int CMD_SIZE = 80;
-
-int process(void){
-	char cmd[CMD_SIZE + 1];//add space for tombstone
-	while(0 == feof(stdin)){
-		if (0 != scanf("%s",cmd)){
-			float f = -9999999.99;
-			switch(sscanf(cmd,"%g",&f)){
-					case 1: printf("%g -- number\n", f);
-					break;
-					case 0: printf("%s\n",cmd);
-					break;
-					default: printf("sscanf failed --aborting\n"); // aka eof
-						 return(EXIT_FAILURE);
-						 break;
-		}
-	}}return EXIT_SUCCESS;
-	
+int main(void) {
+	printf("hello world");
+	return EXIT_SUCCESS;
 }
